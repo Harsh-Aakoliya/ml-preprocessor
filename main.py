eec53d82dc13474a635f39a83e31748045e9b23d
Aakoliya Harsh <106406594+Harsh-Aakoliya@users.noreply.github.com>
2024-04-18 10:04:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -310,5 +310,53 @@ print(ps.nullValues())
 # ps.normalizeColumn(["experience"])
 # ps.categoricalEncoding("gender")
 print(ps.compressLossy(2))
+
+
+
+#another examples.
+from rhinorefine import Preprocessor
+preprocessor = Preprocessor('file.csv')
+
+
+
+# TODO: Fill with mean, median and mode 
+# preprocessor.fillwithmean("age")
+# preprocessor.fillwithmedian("age")
+# preprocessor.fillwithmode("age")
+
+
+#TODO: Removing columns
+# preprocessor.removeColumn("age")
+
+
+#TODO: finding null values
+#so it will print all the null values from giving csv file
+# print(preprocessor.nullValues())
+
+#TODO: standerisation all the columns
+# preprocessor.standardizeColumn(["age"])
+# preprocessor.standardizeData()
+
+
+#TODO: it will normalize all the columns between [0,1]
+# preprocessor.normalizeColumn(['age'])
+# print(preprocessor.normalizeData())
+
+
+#TODO: categorical encoding i.e. onehot encoding
+# preprocessor.categoricalEncoding("gender")
+
+
+
+preprocessor1=Preprocessor("temp.csv")
+#TODO: K means clustering algorithm. no of clusters we have to pass as an argument
+# print(preprocessor1.compressLossy(3))
+ 
+#TODO: PCA as an argument we have to pass no of dimension in which we have to reduce
+print(preprocessor1.compressNonLossy(2))
+
+
+#finally for saving processed csv file
+preprocessor.save()
 print(ps.compressNonLossy(2)[0],ps.compressNonLossy(2)[1])
 ps.save()
